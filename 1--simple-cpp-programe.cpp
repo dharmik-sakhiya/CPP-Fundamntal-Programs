7528b182ad4e8bf4289b9ded6852587eb8e18d51
Dharmik Sakhiya <dharmiksakhiya137@gmail.com>
2024-01-10 22:58:07 +0530

    Added File

---
+++ 1--simple-cpp-programe.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10, m = 0, k;
+    cout << "Name: Dharmik Sakhiya";
+    cout << "\nAddress: Patel Chowk";
+    cout << "\nValue of Programe\t" << a << "\t" << m << "\t" << k;
+    return 0;
+}
